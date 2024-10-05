1102146fa2dcdfcd18f6de06be77a95ecefb8297
David Vardzelian <david.vardzelian@gmail.com>
2024-10-05 15:55:14 +0400

    read Source file logic

--- CompilerCRB/src/main.c
+++ CompilerCRB/src/main.c
@@ -3,5 +3,16 @@
 
 int main(int argc, char *argv[])
 {
-    
+    if (argc < 2)
+    {
+        printf("Usage: %s <sourcefile> \n", argv[0]);
+        return 1;
+    }
+
+    FILE *source = fopen(argv[1], "r");
+    if (!source)
+    {
+        printf("Error: Could not open source file %s\n", argv[2]);
+        return 1;
+    }
 }
